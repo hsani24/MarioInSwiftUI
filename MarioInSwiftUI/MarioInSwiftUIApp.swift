e6ab14487ea2fb5b711bd305de7ba58444b3b4d8
hsani24 <110380167+hsani24@users.noreply.github.com>
2022-12-16 11:00:57 +0100

    Initial Commit

---
+++ MarioInSwiftUI/MarioInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioInSwiftUIApp.swift
+//  MarioInSwiftUI
+//
+//  Created by Sani I on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MarioInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
